371cc3c5a998afc8aa570ed09a6c93b1c350f914
RF212690 <90518866+RF212690@users.noreply.github.com>
2022-05-09 11:05:53 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,8 +18,10 @@ public class Main {
         for (int i = 0; i < 8; i++) {
             board[i][1]=0;
             board[i][6]=1;
-            board[i][5]=1;
-            board[i][4]=1;
+            board[i][5]=-1;
+            board[i][4]=-1;
+            board[i][3]=-1;
+            board[i][2]=-1;
             if(i<3){board[i][0]=i*2+2;board[i][7]=i*2+3;}
             else {board[i][0]=(7-i)*2+2;board[i][7]=(7-i)*2+3;}
         }
@@ -63,6 +65,6 @@ public class Main {
     public static void setHashTable(){
         Pieces.put(0,"WP");Pieces.put(1,"BP");Pieces.put(2,"WR");Pieces.put(3,"BR");Pieces.put(4,"WN");
         Pieces.put(5,"BN");Pieces.put(6,"WB");Pieces.put(7,"BB");Pieces.put(8,"WK");Pieces.put(9,"BK");
-        Pieces.put(10,"WQ");Pieces.put(11,"BQ");
+        Pieces.put(10,"WQ");Pieces.put(11,"BQ");Pieces.put(-1,"VO");
     }
 }
